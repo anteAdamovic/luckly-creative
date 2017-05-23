6d90e552c64fa8193c11b32cc7ca399146fab701
Ante <ante.adamovic93@gmail.com>
2017-05-23 08:53:00 +0200

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+
+import { HomeModule } from './modules/home/home.module';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +20,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
